dc49b9b726b6b628f192bc2abebbbc4d6cb747cb
mihai-stoenica <stoenica.mihai2016@gmail.com>
2025-09-20 14:22:24 +0300

    hided auth forms in case of user is authenticated

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,9 +1,15 @@
 import AuthForm from "../../components/Auth/AuthForm.tsx";
+import { useAuth } from "../../contexts/AuthContext.tsx";
 
 const Login = () => {
+  const { user } = useAuth();
   return (
     <div className="flex flex-col items-center">
-      <AuthForm mode="login" />
+      {!user ? (
+        <AuthForm mode="login" />
+      ) : (
+        <h1 className="mt-4">You are logged in as {user.email}</h1>
+      )}
     </div>
   );
 };
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,9 +1,15 @@
 import AuthForm from "../../components/Auth/AuthForm.tsx";
+import { useAuth } from "../../contexts/AuthContext.tsx";
 
 const Register = () => {
+  const { user } = useAuth();
   return (
     <div className="flex flex-col items-center">
-      <AuthForm mode="register" />
+      {!user ? (
+        <AuthForm mode="register" />
+      ) : (
+        <h1 className="mt-4">You are logged in as {user.email}</h1>
+      )}
     </div>
   );
 };
